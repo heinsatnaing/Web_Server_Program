80a86d68d0ea50325e13bfe00216e585fe603056
Hein Sat Naing <50688597+heinsatnaing@users.noreply.github.com>
2020-01-12 21:34:20 +0700

    Update mysql_install.sh

--- mysql_install.sh
+++ mysql_install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+yum install @mysql -y --allowerasing
+
+systemctl start mysqld
+systemctl enable --now mysqld
+
 echo "Mysql_secure_installation will be started"
 sleep 1
 mysql_secure_installation
